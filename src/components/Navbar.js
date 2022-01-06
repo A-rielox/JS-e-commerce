668f83fb72ee8dfdb28e038814cc5568351e68a5
Ariel <arielox.ag@gmail.com>
2022-01-06 14:32:10 -0300

    2022-01-06-14:31

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,33 +12,35 @@ const Nav = () => {
    const { openSidebar } = useProductsContext();
 
    return (
-      <NavContainer>
-         <div className="nav-center">
-            <div className="nav-header">
-               <Link to="/">
-                  <img src={logo} alt="comfy sloth" />
-               </Link>
+      <React.StrictMode>
+         <NavContainer>
+            <div className="nav-center">
+               <div className="nav-header">
+                  <Link to="/">
+                     <img src={logo} alt="comfy sloth" />
+                  </Link>
 
-               <button type="button" className="nav-toggle">
-                  <FaBars onClick={openSidebar} />
-               </button>
+                  <button type="button" className="nav-toggle">
+                     <FaBars onClick={openSidebar} />
+                  </button>
+               </div>
+
+               <ul className="nav-links">
+                  {links.map(link => {
+                     const { id, text, url } = link;
+
+                     return (
+                        <li key={id}>
+                           <Link to={url}>{text}</Link>
+                        </li>
+                     );
+                  })}
+               </ul>
+
+               <CartButtons />
             </div>
-
-            <ul className="nav-links">
-               {links.map(link => {
-                  const { id, text, url } = link;
-
-                  return (
-                     <li key={id}>
-                        <Link to={url}>{text}</Link>
-                     </li>
-                  );
-               })}
-            </ul>
-
-            <CartButtons />
-         </div>
-      </NavContainer>
+         </NavContainer>
+      </React.StrictMode>
    );
 };
 
--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,20 +1,25 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
 import reducer from '../reducers/products_reducer';
-import { products_url as url } from '../utils/constants';
+import { products_url as url } from '../utils/constants.js';
+
 import {
    SIDEBAR_OPEN,
    SIDEBAR_CLOSE,
-   /*GET_PRODUCTS_BEGIN,
+   GET_PRODUCTS_BEGIN,
    GET_PRODUCTS_SUCCESS,
    GET_PRODUCTS_ERROR,
-   GET_SINGLE_PRODUCT_BEGIN,
+   /*GET_SINGLE_PRODUCT_BEGIN,
    GET_SINGLE_PRODUCT_SUCCESS,
    GET_SINGLE_PRODUCT_ERROR, */
 } from '../actions';
 
 const initialState = {
    isSidebarOpen: false,
+   products_loading: false,
+   products_error: false,
+   products: [],
+   featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -22,6 +27,7 @@ const ProductsContext = React.createContext();
 export const ProductsProvider = ({ children }) => {
    const [state, dispatch] = useReducer(reducer, initialState);
 
+   // ============ SIDEBAR ============
    const openSidebar = () => {
       dispatch({ type: SIDEBAR_OPEN });
    };
@@ -29,6 +35,24 @@ export const ProductsProvider = ({ children }) => {
    const closeSidebar = () => {
       dispatch({ type: SIDEBAR_CLOSE });
    };
+
+   // ============ FETCH PRODUCTS ============
+   const fetchProducts = async url => {
+      dispatch({ type: GET_PRODUCTS_BEGIN });
+
+      try {
+         const response = await axios(url);
+         const products = await response.data;
+
+         dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+      } catch (error) {
+         dispatch({ type: GET_PRODUCTS_ERROR });
+      }
+   };
+
+   useEffect(() => {
+      fetchProducts(url);
+   }, []);
 
    return (
       <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
