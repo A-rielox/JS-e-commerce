a245403567da4d279d86fd00c28e68b62e0cf455
Ariel <arielox.ag@gmail.com>
2022-01-17 17:31:00 -0300

    2022-01-17-17:30

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 import {
+   AuthWrapper,
    Home,
    Products,
    SingleProduct,
@@ -14,40 +15,42 @@ import {
 
 function App() {
    return (
-      <Router>
-         <Navbar />
-         <Sidebar />
+      <AuthWrapper>
+         <Router>
+            <Navbar />
+            <Sidebar />
 
-         <Switch>
-            <Route exact path="/">
-               <Home />
-            </Route>
+            <Switch>
+               <Route exact path="/">
+                  <Home />
+               </Route>
 
-            <Route exact path="/about">
-               <About />
-            </Route>
+               <Route exact path="/about">
+                  <About />
+               </Route>
 
-            <Route exact path="/cart">
-               <Cart />
-            </Route>
+               <Route exact path="/cart">
+                  <Cart />
+               </Route>
 
-            <Route exact path="/products">
-               <Products />
-            </Route>
+               <Route exact path="/products">
+                  <Products />
+               </Route>
 
-            <Route exact path="/products/:id" children={<SingleProduct />} />
+               <Route exact path="/products/:id" children={<SingleProduct />} />
 
-            <PrivateRoute exact path="/checkout">
-               <Checkout />
-            </PrivateRoute>
+               <PrivateRoute exact path="/checkout">
+                  <Checkout />
+               </PrivateRoute>
 
-            <Route exact path="*">
-               <Error />
-            </Route>
-         </Switch>
+               <Route exact path="*">
+                  <Error />
+               </Route>
+            </Switch>
 
-         <Footer />
-      </Router>
+            <Footer />
+         </Router>
+      </AuthWrapper>
    );
 }
 
